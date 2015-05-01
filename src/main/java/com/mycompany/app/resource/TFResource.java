09a2667b4686b13d2e512badb49fd700bc10e2a6
kawulaa <kawulaa@dnb.com>
2015-04-30 20:33:25 -0500

    Returning a response instead of a list of events

--- src/main/java/com/mycompany/app/resource/TFResource.java
+++ src/main/java/com/mycompany/app/resource/TFResource.java
@@ -2,16 +2,14 @@ package com.mycompany.app.resource;
 
 
 
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.codahale.metrics.annotation.Timed;
-import com.mycompany.app.core.Event;
 import com.mycompany.app.core.EventParameters;
 import com.mycompany.app.core.EventParameters.EventParameterBuilder;
 import com.mycompany.app.service.EventService;
@@ -29,9 +27,10 @@ public class TFResource {
 	
 	@GET
 	@Timed
-	public List<Event> getEvent(@QueryParam("zip") String zip) {
+	public Response getEvent(@QueryParam("zip") String zip) {
 		EventParameterBuilder epb = new EventParameterBuilder().withZip(zip);
-		return eventService.findEvents(new EventParameters(epb));
+		return Response.status(Response.Status.OK)
+				.entity(eventService.findEvents(new EventParameters(epb))).build();
 	}
 	
 }
